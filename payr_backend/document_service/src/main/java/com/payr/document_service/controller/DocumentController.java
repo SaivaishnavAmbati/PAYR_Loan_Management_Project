aaba8f053d67a61df743c281a7d389189cd1b074
SaiVaishnavAmbati <166113802+SaivaishnavAmbati@users.noreply.github.com>
2026-02-09 06:39:34 +0530

    Add upload and download endpoints in DocumentController

--- payr_backend/document_service/src/main/java/com/payr/document_service/controller/DocumentController.java
+++ payr_backend/document_service/src/main/java/com/payr/document_service/controller/DocumentController.java
@@ -1,4 +1,33 @@
 package com.payr.document_service.controller;
 
+import com.example.documentService.service.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
 public class DocumentController {
+
+   @Autowired
+    private DocumentService documentService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("file")MultipartFile file) throws IOException{
+        documentService.uploadFile(file);
+        return ResponseEntity.ok("File Uploaded successfully");
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<byte[]> download(@PathVariable String filename){
+        byte[] data = documentService.downloadFile(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+                .body(data);
+    }
+  
+  
 }
